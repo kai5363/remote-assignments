f9b0258e0886685935fa1e5618dce435abd9ba61
kai5363 <57265307+kai5363@users.noreply.github.com>
2022-06-07 14:39:00 +0800

    assignment-3

---
+++ Week-1/Assignment-3/app.js
@@ -0,0 +1,21 @@
+function countAandB(input) {
+    let count = 0;
+    input.forEach(element => {
+        if (element === 'a' || element === 'b') count++;
+    });
+    return count;
+}
+
+function toNumber(input) {
+    return input.map((value, index) => {
+        return input[index] = value.charCodeAt(0) - 96;
+    });;
+}
+
+let input1 = ['a', 'b', 'c', 'a', 'c', 'a', 'c'];
+console.log(countAandB(input1));
+console.log(toNumber(input1));
+
+let input2 = ['e', 'd', 'c', 'd', 'e'];
+console.log(countAandB(input2));
+console.log(toNumber(input2));
